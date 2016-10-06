280ab99c92caf74a20d2443befccaf6d9a484283
Steingrímur <steingrimur@740.is>
2016-10-06 23:07:00 +0000

    Swimming pools page styled

--- single-sundlaugar.php
+++ single-sundlaugar.php
@@ -1,9 +1,49 @@
 <?php include('header.php'); ?>
 
-<div class="a">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h2><?php the_title(); ?>aaaa</h2>
-  <?php endwhile; endif; ?>
-</div>
+<?php
+  $phoneIcon = "phone-call.svg";
+  $emailIcon = "email.svg";
+  $webIcon = "worldwide.svg";
+?>
+
+<!-- <div class="latest-posts wrapper"> -->
+  <div class="swp-single-posts-area wrapper">
+    <div class="swp-single-post">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="swp-headlines">
+        <h2><?php the_field('field_57f40974fd4fa'); ?></h2>
+        <h4><?php the_field('field_57f546e7798b8'); ?></h4>
+      </div>
+
+      <div class="swp-images swp-images-display">
+        <?php the_field('field_57f54ff01c83b'); ?>
+      </div>
+
+      <div class="swp-contact-info mobile-width">
+        <p><?php get_template_part('assets/svg/inline', $phoneIcon); ?><?php the_field('field_57f561fd0c57b'); ?></p>
+        <p><?php get_template_part('assets/svg/inline', $emailIcon); ?><?php the_field('field_57f561e00c57a'); ?></p>
+        <p><?php get_template_part('assets/svg/inline', $webIcon); ?><?php the_field('field_57f5620d0c57c'); ?></p>
+      </div>
+
+      <div class="swp-opening-hours mobile-width">
+        <?php the_field('field_57f409b5fd4fb'); ?>
+      </div>
+
+      <div class="swp-price-list mobile-width">
+        <?php the_field('field_57f5498f798b9'); ?>
+      </div>
+
+      <div class="swp-images swp-images-mobile">
+        <?php the_field('field_57f54ff01c83b'); ?>
+      </div>
+
+      <div class="swp-description">
+        <?php the_field('field_57f409fefd4fc'); ?>
+      </div>
+
+    <?php endwhile; endif; ?>
+  </div>
+  </div>
+<!-- </div> -->
 
 <?php include('footer.php'); ?>
