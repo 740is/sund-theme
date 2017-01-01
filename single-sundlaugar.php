319e12536f06c9ed102815831f422e4eb4736105
Steingrímur <steingrimur@740.is>
2017-01-01 16:09:35 +0000

    ACF Table field installed and styled

--- single-sundlaugar.php
+++ single-sundlaugar.php
@@ -28,18 +28,79 @@
       </div>
 
       <div class="swp-opening-hours mobile-width">
+        <?php
+          $field_key = 'field_57f409b5fd4fb';
+          $fieldLable = get_field_object($field_key);
+          if( $fieldLable ):
+            echo '<h4>' . $fieldLable['label'] . '</h4>';
+           endif;
+
+        ?>
+
         <?php the_field('field_57f409b5fd4fb'); ?>
       </div>
 
       <div class="swp-price-list mobile-width">
-        <?php the_field('field_57f5498f798b9'); ?>
+        <?php
+        $table = get_field( 'field_57f5498f798b9' );
+
+          $field_key = 'field_57f5498f798b9';
+          $fieldLable = get_field_object($field_key);
+          if( $fieldLable ):
+            echo '<h4>' . $fieldLable['label'] . '</h4>';
+           endif;
+
+        if ( $table ) {
+            echo '<table border="0">';
+                if ( $table['header'] ) {
+                    echo '<thead>';
+                        echo '<tr>';
+                            foreach ( $table['header'] as $th ) {
+
+                                echo '<th>';
+                                    echo $th['c'];
+                                echo '</th>';
+                            }
+                        echo '</tr>';
+                    echo '</thead>';
+                }
+                echo '<tbody>';
+                    foreach ( $table['body'] as $tr ) {
+                        echo '<tr>';
+                            foreach ( $tr as $td ) {
+                                echo '<td>';
+                                    echo $td['c'];
+                                echo '</td>';
+                            }
+                        echo '</tr>';
+                    }
+                echo '</tbody>';
+            echo '</table>';
+        }
+        ?>
       </div>
 
       <div class="swp-images swp-images-mobile">
+        <?php
+          $field_key = 'field_57f54ff01c83b';
+          $fieldLable = get_field_object($field_key);
+          if( $fieldLable ):
+            echo '<h4>' . $fieldLable['label'] . '</h4>';
+           endif;
+        ?>
+
         <?php the_field('field_57f54ff01c83b'); ?>
       </div>
 
       <div class="swp-description">
+        <?php
+          $field_key = 'field_57f409fefd4fc';
+          $fieldLable = get_field_object($field_key);
+          if( $fieldLable ):
+            echo '<h4>' . $fieldLable['label'] . '</h4>';
+           endif;
+        ?>
+
         <?php the_field('field_57f409fefd4fc'); ?>
       </div>
       <div id="google-map"></div>
