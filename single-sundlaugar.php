a1625db4d9510b8c84eddc98ae1eb70f5b18921b
Steingrímur <steingrimur@740.is>
2017-01-08 20:09:05 +0000

    Minor fixes

--- single-sundlaugar.php
+++ single-sundlaugar.php
@@ -1,6 +1,10 @@
 <?php include('header.php'); ?>
 
 <?php
+  $facebookUrl = get_field('field_5850550b2b2be');
+  $emailUrl = get_field('field_57f561e00c57a');
+  $webUrl = get_field('field_57f5620d0c57c');
+
   $phoneIcon = "phone-call.svg";
   $emailIcon = "email.svg";
   $webIcon = "worldwide.svg";
@@ -22,9 +26,47 @@
 
       <div class="swp-contact-info mobile-width">
         <p><?php get_template_part('assets/svg/inline', $phoneIcon); ?><?php the_field('field_57f561fd0c57b'); ?></p>
-        <p><?php get_template_part('assets/svg/inline', $emailIcon); ?><a href="mailto:<?php the_field('field_57f561e00c57a'); ?>?subject=Fyrirspurn%20af%20sundlaugar.is%20/%20Inquiry%20from%20swimminginiceland.is"><?php the_field('field_57f561e00c57a'); ?></a></p>
-        <p><?php get_template_part('assets/svg/inline', $webIcon); ?><a href="<?php the_field('field_57f5620d0c57c'); ?>" target="_blank"><?php the_field('field_585060433a3e3'); ?></a></p>
-        <p><?php get_template_part('assets/svg/inline', $facebookIcon); ?><a href="<?php the_field('field_5850550b2b2be'); ?>" target="_blank"><?php the_field('field_58505810f64c3'); ?></a></p>
+        <?php
+          if(!$emailUrl) {
+            echo '';
+          } else {
+            echo '<p>';
+            get_template_part('assets/svg/inline', $emailIcon);
+            echo '<a href="mailto:';
+            the_field('field_57f561e00c57a');
+            echo '?subject=Fyrirspurn%20af%20sundlaugar.is%20/%20Inquiry%20from%20swimminginiceland.is">';
+            the_field('field_57f561e00c57a');
+            echo '</a></p>';
+          }
+        ?>
+
+        <?php
+          if(!$webUrl) {
+            echo '';
+          } else {
+            echo '<p>';
+            get_template_part('assets/svg/inline', $webIcon);
+            echo '<a href="';
+            the_field('field_57f5620d0c57c');
+            echo '" target="_blank">';
+            the_field('field_585060433a3e3');
+            echo '</a></p>';
+          }
+        ?>
+
+        <?php
+          if(!$facebookUrl) {
+            echo '';
+          } else {
+            echo '<p>';
+            get_template_part('assets/svg/inline', $facebookIcon);
+            echo '<a href="';
+            the_field('field_5850550b2b2be');
+            echo '" target="_blank">';
+            the_field('field_58505810f64c3');
+            echo '</a></p>';
+          }
+        ?>
       </div>
 
       <div class="swp-opening-hours mobile-width">
