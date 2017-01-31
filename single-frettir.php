66d64f5c58f6895a2ef54ab8222354461c49123b
Steingrímur <steingrimur@740.is>
2017-01-31 23:42:45 +0000

    News pages adjustments
    Ad section in place and some margin adjustments.

--- single-frettir.php
+++ single-frettir.php
@@ -11,30 +11,30 @@
 
     	while ( have_posts() ) {
     		the_post();
-          $frettirBanner = "field_5862de561fe26";
-          $frettirBannerUrl = "field_5862df271fe27";
           $frettirTextTwo = "field_5862e0f0e2985";
 
           $post_id = get_the_ID();
 
-          $frettirBannerImage = get_field($frettirBanner, $post_id);
-          $frettirBannerTheUrl = get_field($frettirBannerUrl, $post_id);
           $frettirTextTwoText = get_field($frettirTextTwo, $post_id);
 
           echo '<div class="single-posts-area">';
-          include('swimmer.php');
+
             echo '<div id="frettir" class="single-post">';
+              include('ad-banner-1.php');
               echo '<h2>';
                 echo the_title();
               echo '</h2>';
               echo '<i class="latest-post-date">';
                 echo get_the_date('d/m/Y');
               echo '</i>';
+              include('swimmer.php');
               echo the_content();
-              include('ad-banner-1.php');
-              // echo '<a href="' . $frettirBannerTheUrl . '" target="_blank">';
-              // echo '<img id="frettir-ad-banner" src="' . $frettirBannerImage['url'] . '" alt="' . $frettirBannerImage['alt'] . '">';
-              // echo '</a><p></p>';
+              echo '<div class="ad-cubes-area">';
+                echo '<div class="ad-cubes-area-content">';
+                  include('ad-cube-1.php');
+                  include('ad-cube-2.php');
+                echo '</div>';
+              echo '</div>';
               echo '<div class="frettir-texti-tvo">';
               echo $frettirTextTwoText;
               echo '</div>';
@@ -47,15 +47,15 @@
     } else {
     	_e( 'Því miður er ekkert til að birta hér.' );
     }
-    posts_nav_link();
+    //posts_nav_link();
     echo '<div class="previous-link">';
       previous_post_link('%link', '<< Fyrri');
     echo '</div>';
     echo '<div class="next-link">';
       next_post_link('%link', 'Næsta >>');
     echo '</div>';
-    echo '</div>';
-    echo '</div>';
+    // echo '</div>';
+    // echo '</div>';
     ?>
 
 
